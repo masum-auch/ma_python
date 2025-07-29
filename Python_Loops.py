df5e7b072169699f0a67b0103e9463140a8d563c
Masum <masumfaa69@gmail.com>
2025-07-29 11:55:33 +0600

    update file

---
+++ Python_Loops.py
@@ -0,0 +1,23 @@
+# count = 0
+# while count <6:
+#     print("hello")
+#     count += 1
+# print(count)
+
+# i = 1
+# while i <= 10000:
+#     print(i)
+#     i += 1
+# print("Loops End")
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+# print("Loops End")
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1 
+# print("loop end") 
